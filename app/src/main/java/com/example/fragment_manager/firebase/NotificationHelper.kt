a7d0e5ba03495810b42d3da5bb00013f24643549
Aydar <mr.ivaoimop@mail.ru>
2024-05-20 19:58:12 +0300

    SI-9574 Настройка каналов уведомлений

--- app/src/main/java/com/example/fragment_manager/firebase/NotificationHelper.kt
+++ app/src/main/java/com/example/fragment_manager/firebase/NotificationHelper.kt
@@ -1,9 +1,9 @@
 package com.example.fragment_manager.firebase
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import androidx.core.app.NotificationCompat
+import com.example.fragment_manager.App
 import com.example.fragment_manager.R
 import com.google.firebase.messaging.RemoteMessage
 
@@ -14,29 +14,24 @@ class NotificationHelper(private val context: Context) {
     }
 
     fun showNotification(remoteMessage: RemoteMessage) {
-        createNotificationChannel()
-        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.fun_1)
-            .setContentTitle(remoteMessage.notification?.title)
-            .setContentText(remoteMessage.notification?.body)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setAutoCancel(true)
+        val channel = App.CHANNEL_FOR_TAB_FRAGMENTS_ID
+        if (!checkIsChannelEnable(channel)) {
+            return
+        }
+        val notificationBuilder =
+            NotificationCompat.Builder(context, channel)
+                .setSmallIcon(R.drawable.fun_1)
+                .setContentTitle(remoteMessage.notification?.title)
+                .setContentText(remoteMessage.notification?.body)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
 
         val notificationId = System.currentTimeMillis().toInt()
         notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
-    private fun createNotificationChannel() {
-        val name = "Default Channel"
-        val descriptionText = "Default notification channel"
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-            description = descriptionText
-        }
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    companion object {
-        private const val CHANNEL_ID = "default_channel_id"
+    private fun checkIsChannelEnable(channelId: String): Boolean {
+        val channel = notificationManager.getNotificationChannel(channelId)
+        return channel != null && channel.importance != NotificationManager.IMPORTANCE_NONE
     }
 }
